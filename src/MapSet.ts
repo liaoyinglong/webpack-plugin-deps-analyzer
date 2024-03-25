d6d51bddcc6b9f41938442bd58f2e7b87924d38a
liaoyinglong <vigossliao@gmail.com>
2024-03-25 21:47:46 +0800

    update

--- src/MapSet.ts
+++ src/MapSet.ts
@@ -1,4 +1,7 @@
-export class MapSet<K extends string = string, V = string> extends Map<K, Set<V>> {
+export class MapSet<
+  K extends string = string,
+  V extends string = string,
+> extends Map<K, Set<V>> {
   append(key: K, value: V) {
     let s = this.get(key);
     if (!s) {
@@ -10,9 +13,15 @@ export class MapSet<K extends string = string, V = string> extends Map<K, Set<V>
 
   toJson() {
     const r = {} as Record<K, V[]>;
-    for (const [key, value] of this) {
-      r[key] = [...value];
-    }
+
+    // 用 en 的方式排序
+    const keys = [...this.keys()].sort((a, b) => a.localeCompare(b, "en"));
+
+    keys.forEach((k) => {
+      const v = this.get(k)!;
+      r[k] = [...v].sort((a, b) => a.localeCompare(b, "en"));
+    });
+
     return r;
   }
 }
